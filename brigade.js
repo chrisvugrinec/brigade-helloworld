9d4c224e00a5124782dd57bd6dd57c3eca54ba66
gogsadmin <RedHat$1>
2018-07-24 08:01:25 +0200

    test

--- brigade.js
+++ brigade.js
@@ -1,5 +1,5 @@
 const { events } = require('brigadier')
 
 events.on("exec", (brigadeEvent, project) => {
-  console.log("Hello world! FROM GITHUB")
+  console.log("Hello world! FROM GITHUB...now changed and commited")
 })
