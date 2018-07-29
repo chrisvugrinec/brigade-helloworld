969565861ca1dc88a2ae3d3769996f012b49f51a
gogsadmin <RedHat$1>
2018-07-29 16:16:51 +0200

    test

--- brigade.js
+++ brigade.js
@@ -4,5 +4,5 @@ const { events } = require("brigadier")
 events.on("exec", () => {
   j = new Job("example")
   j.env= {"MY_ENV_VAR": p.secrets.myVar}
-  console.log("Hello world, secret value: "$MY_ENV_VAR)
+  console.log("Hello world, secret value: "+$MY_ENV_VAR)
 })
