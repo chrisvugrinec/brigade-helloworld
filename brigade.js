299ebe6baa174d22d2e943ae20332c70cb3df87d
gogsadmin <RedHat$1>
2018-07-29 16:40:22 +0200

    added param

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ const { events, Job } = require("brigadier")
 
 events.on("exec", (event, project) => {
   var hello = new Job("hello", "alpine:3.4")
-  hello.env= {"MY_ENV_VAR": project.secrets.myVar}
+  hello.env= {"MY_ENV_VAR": project.secrets.username}
   hello.tasks = [
     "echo Hello",
     "echo World $MY_ENV_VAR"
